baf796c89541c049f725129e7c79e68c640061f9
yuyang <421736079@qq.com>
2018-03-04 13:42:12 +0800

    comment: src/platforms/web/compiler/util.js

--- src/platforms/web/compiler/util.js
+++ src/platforms/web/compiler/util.js
@@ -1,7 +1,11 @@
 /* @flow */
+// CHECKOUT 
 
 import { makeMap } from 'shared/util'
 
+/**zh-cn
+ * 用于匹配自闭合html标签
+ */
 export const isUnaryTag = makeMap(
   'area,base,br,col,embed,frame,hr,img,input,isindex,keygen,' +
   'link,meta,param,source,track,wbr'
@@ -9,12 +13,19 @@ export const isUnaryTag = makeMap(
 
 // Elements that you can, intentionally, leave open
 // (and which close themselves)
+/**zh-cn
+ * 闭合标签可以省略
+ */
 export const canBeLeftOpenTag = makeMap(
   'colgroup,dd,dt,li,options,p,td,tfoot,th,thead,tr,source'
 )
 
 // HTML5 tags https://html.spec.whatwg.org/multipage/indices.html#elements-3
 // Phrasing Content https://html.spec.whatwg.org/multipage/dom.html#phrasing-content
+/**zh-cn
+ * 包含[html5](https://html.spec.whatwg.org/multipage/indices.html#elements-3)和[whatwg Phrasing Content](Phrasing Content https://html.spec.whatwg.org/multipage/dom.html#phrasing-content)中
+ * 所有必须闭合的标签
+ */
 export const isNonPhrasingTag = makeMap(
   'address,article,aside,base,blockquote,body,caption,col,colgroup,dd,' +
   'details,dialog,div,dl,dt,fieldset,figcaption,figure,footer,form,' +
